396674ba1cc8653b5ffe0ca50406c6acbdc00bc1
Elvin1999 <elvincamalzade00@gmail.com>
2023-06-27 18:00:18 +0400

    Lesson 2 things

---
+++ WebApiDemoAK/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiDemoAK.Dtos;
+
+namespace WebApiDemoAK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        [HttpGet("")]
+        public List<ContactDTO> GetContacts()
+        {
+            return new List<ContactDTO>
+            {
+                new ContactDTO
+                {
+                    Id=1,Firstname="Tural",Lastname="Eliyev",PAN="4169855878789596"
+                },
+                new ContactDTO
+                {
+                    Id=2,Firstname="Mike",Lastname="Mammadli",PAN="4169225878789596"
+                }
+            };
+        }
+        //localhost:5050/Contact/First
+        [HttpGet("First")]
+        public ContactDTO GetFirst()
+        {
+            return new ContactDTO
+            {
+                Id = 1,
+                Firstname = "Tural",
+                Lastname = "Eliyev",
+                PAN = "4169855878789596"
+            };
+        }
+    }
+}
---
+++ WebApiDemoAK/Dtos/ContactDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiDemoAK.Dtos
+{
+    public class ContactDTO
+    {
+        public int Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? PAN { get; set; }
+    }
+}
+
